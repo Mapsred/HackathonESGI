cb398dbc46c333214352507895064bb2c0864014
François MATHIEU <maps_red@yahoo.fr>
2018-03-20 16:03:37 +0100

    Add AddIntentCommand

---
+++ src/Command/AddIntentCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Intent;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class AddIntentCommand extends ContainerAwareCommand
+{
+    protected static $defaultName = 'app:add-intent';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('name', InputArgument::REQUIRED, 'The intent name')
+            ->addArgument('parameters', InputArgument::IS_ARRAY, 'An array of parameters');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $name = $input->getArgument('name');
+        $parameters = $input->getArgument('parameters');
+
+        $intent = new Intent();
+        $intent->setName($name)->setParameters($parameters);
+
+        $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $manager->persist($intent);
+        $manager->flush();
+
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    }
+}
--- src/Entity/Intent.php
+++ src/Entity/Intent.php
@@ -5,50 +5,72 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="intent")
  * @ORM\Entity(repositoryClass="App\Repository\IntentRepository")
  */
 class Intent
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @var int $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string $name
+     * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
 
     /**
-     * @ORM\Column(type="json_array")
+     * @var array $parameters
+     * @ORM\Column(name="parameters", type="array")
      */
     private $parameters;
 
+    /**
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    /**
+     * @return string
+     */
+    public function getName()
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function setName($name)
     {
         $this->name = $name;
 
         return $this;
     }
 
+    /**
+     * @return array
+     */
     public function getParameters()
     {
         return $this->parameters;
     }
 
-    public function setParameters($parameters): self
+    /**
+     * @param $parameters
+     * @return $this
+     */
+    public function setParameters($parameters)
     {
         $this->parameters = $parameters;
 
