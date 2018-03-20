c0a987d50ba2b7719eb0ee8ef1e17190c8cfd6f8
Alexandre Parizot <alexandre.parizot@hotmail.fr>
2018-03-20 15:19:07 +0100

    ajax dans script.js

--- assets/js/default/ajax.js
+++ assets/js/default/ajax.js
@@ -1,12 +1,7 @@
+/*
+
 $("button").click(function() {
 
-    $.ajax({
-        type: "POST",
-        url: "/query?q=",
-        dataType: 'json',
-        success: function(reponse){
-            console.log(reponse);
-        }
-    });
+    var query = ;
 
-});
+*/
