61e6195e64943d50123334c201cee64ccaf99dd3
François MATHIEU <maps_red@yahoo.fr>
2018-03-23 12:10:11 +0100

    Fix app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
 require('../bower/jquery/dist/jquery.js');
 require('../bower/bootstrap/dist/js/bootstrap.js');
-require('../bower/materialize/dist/js/materialize.js');
 require('../js/default/script.js');
